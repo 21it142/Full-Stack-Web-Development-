8d40242f847634ecf06c4c82fe66eb3471f8751a
Jay R Shah <97278175+21it142@users.noreply.github.com>
2023-07-17 21:33:29 +0530

    Update task-6.js

--- FSWD/task-6.js
+++ FSWD/task-6.js
@@ -21,5 +21,25 @@ fs.readFile("task-6.txt",'utf-8',function(err,file){
     else{
         console.log("Task-6.txt is larger then other file")
     }
+    const lines1 = fs.readFileSync('student-data.txt', 'utf8').split('\n');
+
+    // Read the contents of the second file
+    const lines2 = fs.readFileSync('task-6.txt', 'utf8').split('\n');
+  
+    // Compare the sizes of the files
+    const file1Size = lines1.length;
+    const file2Size = lines2.length;
+    const differingLines = [];
+    for (let i = 0; i < Math.min(file1Size, file2Size); i++) {
+      if (lines1[i] !== lines2[i]) {
+        differingLines.push(i + 1); // Add 1 to convert from zero-based index to line number
+      }
+    }
+  
+    if (differingLines.length > 0) {
+        console.log('The following lines are different:');
+        differingLines.forEach(line => {
+          console.log(`Line ${line}`);
+        });
 });
 });
